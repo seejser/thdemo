b9b24f0d25c67e324b78c76f4775048d43c1bc4c
hooperhu <hooper@qq.com>
2025-11-04 15:54:48 +0800

    add

--- my-vue-app/README.md
+++ my-vue-app/README.md
@@ -1,5 +1,23 @@
-# Vue 3 + Vite
+# Vue 3 + Vite+vue-router@4
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+## 命令
 
-Learn more about IDE Support for Vue in the [Vue Docs Scaling up Guide](https://vuejs.org/guide/scaling-up/tooling.html#ide-support).
+```sh
+# 进入项目
+cd my-vue-app
+#安装依赖
+npm i
+#启动
+npm run dev
+```
+
+## 安装
+
+```sh
+# 全局安装：@nutui/nutui
+npm i @nutui/nutui
+# vue-router@4
+npm install vue-router@4
+#启动
+npm run dev
+```
--- my-vue-app/src/pages/Home.vue
+++ my-vue-app/src/pages/Home.vue
@@ -24,12 +24,17 @@
           </div>
         </nut-pull-refresh>
       </nut-infinite-loading>
+      <nut-tabbar v-model="active" bottom safe-area-inset-bottom placeholder>
+    <nut-tabbar-item v-for="(item, index) in List" :key="index" :tab-title="item.title" :icon="item.icon">
+    </nut-tabbar-item>
+  </nut-tabbar>
     </div>
   </nut-config-provider>
 </template>
 
 <script setup>
-import { ref, computed } from "vue";
+import { h,ref, computed } from "vue";
+import { Home, Category, Find, Cart, My } from '@nutui/icons-vue'
 
 const theme = ref("");
 const checked = ref(false);
@@ -40,6 +45,7 @@ const tabsValue = ref(0);
 const sum = ref(24);
 const infinityValue = ref(false);
 const hasMore = ref(true);
+const active = ref(0)
 const loadMore = () => {
   setTimeout(() => {
     sum.value += 24;
@@ -58,6 +64,29 @@ const refreshFun = () => {
 const change = (v) => {
   theme.value = v ? "dark" : "light";
 };
+const List = [
+  {
+    title: 'Home',
+    icon: h(Home)
+  },
+  {
+    title: 'Category',
+    icon: h(Category)
+  },
+  {
+    title: 'Find',
+    icon: h(Find)
+  },
+  {
+    title: 'Cart',
+    icon: h(Cart)
+  },
+  {
+    title: 'My',
+    icon: h(My)
+  }
+]
+
 </script>
 
 <style>
